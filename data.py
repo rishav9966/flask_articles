c4f7fdf2001989965cfa1c5827e3b36aef705862
rishav9966 <39216235+rishav9966@users.noreply.github.com>
2020-06-25 11:48:15 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title':'Article one',
+            'body':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia blanditiis illum rerum quidem repellat beatae eaque obcaecati perspiciatis reiciendis optio quod culpa fugit dicta veniam, rem accusamus provident? Numquam, facere.',
+            'author':'Rishav',
+            'create_date':'05-21-2020'
+        },
+        {
+            'id' : 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia blanditiis illum rerum quidem repellat beatae eaque obcaecati perspiciatis reiciendis optio quod culpa fugit dicta veniam, rem accusamus provident? Numquam, facere.',
+            'author':'Ritu',
+            'create_date':'05-21-2020'
+        },
+        {
+            'id' : 3,
+            'title':'Article Three',
+            'body':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia blanditiis illum rerum quidem repellat beatae eaque obcaecati perspiciatis reiciendis optio quod culpa fugit dicta veniam, rem accusamus provident? Numquam, facere.',
+            'author':'Rishav',
+            'create_date':'05-21-2020'
+        }
+    ]
+    return articles
